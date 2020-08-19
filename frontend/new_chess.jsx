576c64cb19f4a87e6b3dde5fdca719b1ddde5d98
Jim-Heftypants <nickcsercel@gmail.com>
2020-08-18 17:14:02 -0700

    test addition

--- frontend/new_chess.jsx
+++ frontend/new_chess.jsx
@@ -6,7 +6,7 @@ import configureStore from "./store/store";
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     const store = configureStore();
-
+    // comment for git add .
     window.store = store;
 
     ReactDOM.render(<Root store={store} />, root);
